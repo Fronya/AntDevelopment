84416df2a8d95db86b330b13cb97136e5e122cb0
Fronya <gubareva_olga@outlook.com>
2017-04-28 14:27:14 +0300

    Initial commit

---
+++ src/main/java/com/fronya/ant/task/RenameTask.java
@@ -0,0 +1,54 @@
+package com.fronya.ant.task;
+
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
+
+import java.io.File;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RenameTask extends Task{
+    private String destDir;
+    private String jobId;
+    private List<FileSet> filesets = new LinkedList<>();
+
+    public void setDestDir(String destDir) {
+        this.destDir = destDir;
+    }
+
+    public void setJobId(String jobId) {
+            this.jobId = jobId;
+    }
+
+    public void addFileset(FileSet fileset){
+        filesets.add(fileset);
+    }
+
+    public void execute() throws BuildException {
+        for (FileSet currentFileSet: filesets) {
+            DirectoryScanner ds = currentFileSet.getDirectoryScanner(getProject());         // 3
+            String[] includedFiles = ds.getIncludedFiles();
+            for (String currentFile: includedFiles) {
+                if(!renameFile(currentFile)){
+                    throw new BuildException();
+                }
+            }
+        }
+    }
+
+    private boolean renameFile(String nameOldFile){
+        StringBuilder fullNameFile = new StringBuilder(destDir);
+        fullNameFile.append(File.separator).append(nameOldFile);
+        File oldFile = new File(fullNameFile.toString());
+
+        int indPoint = fullNameFile.lastIndexOf(".");
+        fullNameFile.insert(indPoint, '-').insert(indPoint + 1, jobId);
+        File newFile = new File(fullNameFile.toString());
+        return oldFile.renameTo(newFile);
+    }
+}
